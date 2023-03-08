7f9cac468d58a4aeb2c9bb5cc7b745a985b5d5cb
sakioo <53650208+thomaue@users.noreply.github.com>
2023-03-08 15:34:08 +0100

    Update ft_atoi.c

--- Level_2/ft_atoi/ft_atoi.c
+++ Level_2/ft_atoi/ft_atoi.c
@@ -1,27 +1,64 @@
 // #include <stdio.h>
 
-int	ft_atoi(const char *str)
+int ft_atoi(char * str)
 {
+    int signe;
     int i;
     int num;
 
+    signe = 0;
     i = 0;
     num = 0;
 
-    while (str[i] != '\0')
+    if (str[0] == '\0')
     {
-        while (str[i] <= '9' && str[i] >= '0')
+        return 0;
+    }
+
+    else
+    {
+        while(str[i] != '\0')
         {
-            num = (num * 10) + str[i] - 48;
-            i+=1;
+            if (str[i] == '-' || str[i] == '+' || str[i] == ' ' || str[i] == '\f' || str[i] == '\n' || str[i] == '\r' || str[i] == '\t' || str[i] == '\v' )
+            {
+                if (str[i] == '-')
+                {
+                    signe = signe + 1;
+                }
+                i++;
+            }
+
+            else if (str[i] >= '0' && str[i] <= '9')
+            {
+                while (str[i] >= '0' && str[i] <= '9')
+                {
+                    num = (num * 10) + str[i] - 48;
+                    i++;
+                }
+                break;
+            }
+
+            else
+            {
+                return 0;
+            }
+        }
+        
+        if (signe % 2 ==0)
+        {
+            return (num);
+        }
+        
+        else if (signe % 2==1)
+        {
+            return (num * -1);
         }
     }
-    return num;
 }
 
 // int main(void)
 // {
-//     char num[] = "42545145";
+//    char num[] = "42545145";
 
 //     printf("\n\t%d\n\n", ft_atoi(num));
 
