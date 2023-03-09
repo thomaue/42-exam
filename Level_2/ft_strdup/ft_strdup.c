aa379807b1d7d552b44f6bbdca458a6ee7b6cf06
sakioo <53650208+thomaue@users.noreply.github.com>
2023-03-09 10:31:31 +0100

    Create ft_strdup.c

---
+++ Level_2/ft_strdup/ft_strdup.c
@@ -0,0 +1,33 @@
+// #include <stdio.h>
+// #include <stdlib.h>
+
+char    *ft_strdup(char *src)
+{
+    int i;
+
+    i = 0;
+
+    char *src2 = malloc(sizeof(char));
+
+    if (src2 == NULL)
+    {
+        return NULL;
+    }
+
+    while (src[i] != '\0')
+    {
+        src2[i] = src[i];
+        i++;
+    }
+
+    return src2;
+}
+
+// int main(void)
+// {
+//     char src[] = "Galatée";
+
+//     printf("\n\t%s\n\n", ft_strdup(src));
+
+//     return 0;
+// }
